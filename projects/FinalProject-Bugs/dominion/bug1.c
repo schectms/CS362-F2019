bc87b021793087939a9a605a4c9ed6829cefd7c4
Shifra T Schectman <43650010+schectms@users.noreply.github.com>
2019-12-02 10:28:56 -0500

    Update bug1.c

--- projects/FinalProject-Bugs/dominion/bug1.c
+++ projects/FinalProject-Bugs/dominion/bug1.c
@@ -5,43 +5,42 @@
 
 int main(int argc, char** argv) {
 
-	int seed = 1000;
-	int numPlayers = 2;
-	int thisPlayer = 0;
-	int nextPlayer = 1;
-	int choice1 = 0, choice2 = 0, choice3 = 0, handPos = 0, bonus = 0;
+        int seed = 1000;
+        int numPlayers = 2;
+        int thisPlayer = 0;
+        int nextPlayer = 1;
+        int choice1 = 0, choice2 = 0, choice3 = 0, handPos = 0, bonus = 0;
 
-	struct gameState G, testG; //declare game states
+        struct gameState G, testG; //declare game states
 
-	int k[10] = { adventurer, embargo, village, minion, mine, cutpurse,
-			sea_hag, tribute, smithy, council_room };
+        int k[10] = { adventurer, embargo, village, minion, mine, cutpurse,
+                        sea_hag, tribute, smithy, council_room };
 
-	// initialize a game state and player cards
-	initializeGame(numPlayers, k, seed, &G);
+        // initialize a game state and player cards
+        initializeGame(numPlayers, k, seed, &G);
 
-	// ----------- TEST 1: BUG 1 TRIBUTE CARD NOT DISCARDED--------------
-	printf("TEST 1: BUG 1 TRIBUTE CARD NOT DISCARDED\n");
+        // ----------- TEST 1: BUG 1 TRIBUTE CARD NOT DISCARDED--------------
+        printf("TEST 1: BUG 1 TRIBUTE CARD NOT DISCARDED\n");
 
-	memcpy(&testG, &G, sizeof(struct gameState));
-	G.deck[nextPlayer][1] = mine;
-	G.deck[nextPlayer][0] = copper;
+        memcpy(&testG, &G, sizeof(struct gameState));
+        G.deck[nextPlayer][1] = mine;
+        G.deck[nextPlayer][0] = copper;
 
-	cardEffect(tribute, choice1, choice2, choice3, &testG, handPos, &bonus);
-	//playedCards should contain the tribute card
-	printf("Result of Card Effect= %d, Expected = %d\n", testG.playedCardCount, G.playedCardCount + 1);
+        cardEffect(tribute, choice1, choice2, choice3, &testG, handPos, &bonus);
+        //playedCards should contain the tribute card
+        printf("Played Card Count= %d, Expected = %d\n", testG.playedCardCount, G.playedCardCount + 1);
 
-	// ----------- TEST 2: BUG 1 MINE CARD NOT DISCARDED--------------
-	printf("TEST 1: BUG 1 MINE CARD NOT DISCARDED\n");
+        // ----------- TEST 2: BUG 1 MINE CARD NOT DISCARDED--------------
+        printf("TEST 1: BUG 1 MINE CARD NOT DISCARDED\n");
 
-	choice1 = 2;
-	G.hand[thisPlayer][choice1] = copper;
-	choice2 = steward;
+        choice1 = 2;
+        G.hand[thisPlayer][choice1] = copper;
+        choice2 = steward;
 
-	memcpy(&testG, &G, sizeof(struct gameState));
-	cardEffect(mine, choice1, choice2, choice3, &testG, handPos, &bonus);
-	//trashed card should not be in playedCards
-	printf("Result of Card Effect= %d, Expected = %d\n", testG.playedCardCount, G.playedCardCount +1);
+        memcpy(&testG, &G, sizeof(struct gameState));
+        int x=cardEffect(mine, choice1, choice2, choice3, &testG, handPos, &bonus);
+        //trashed card should not be in playedCards
+        printf("Played Card Count= %d, Expected = %d\n", testG.playedCardCount, G.playedCardCount +1);
 
-	return 0;
+        return 0;
 }
-
